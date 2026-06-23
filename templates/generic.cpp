6fd450e78ae0fa4a49ae45bef08c2af73e8f81a4
Stefanos Stefanou <stefanos.stefanou@century.tech>
2026-06-23 05:23:40 +0100

    feat: Usago Bronze DS

---
+++ templates/generic.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <math.h>
+using namespace std;
+#define ll 		long long
+#define ull 	unsigned long long
+#define ld 		long double
+#define MOD  	pow(10,9)+7;
+#define INF_INT	(1<<30)
+#define INF_LL 	(1L<<62)
+
+//forward refs
+template <typename container> void debug(container& genericSequence,string id="None", int depth=0);
+
+//debug utils
+#ifdef DEBUG
+	template <typename container> void debug(container& genericSequence,string id, int depth){
+		cout<<"=============Debug ("<<id<<") START =============\n";
+		string prefix = "";
+		for(int i=0;i<depth;i++) prefix+="\t";
+
+		for(auto every: genericSequence){
+			cout<<prefix<<every<<"\n";
+		}
+		cout<<"=============Debug ("<<id<<") END =============\n";
+
+	}
+#endif
+#ifndef DEBUG
+	template <typename container> void debug(container& genericSequence,string id, int depth){
+		return ;
+	}
+
+#endif
+
+/**
+ 	*
+	? Stuff to look for ->
+    * stay organised
+    * int overflows, array bounds, etc.
+    * special cases (n=1)?
+    * do something instead of nothing
+    * timebox your approach
+    * simple is better than complex
+    * n % mod = (n % mod + mod) % mod;
+    * long long instead of int
+    
+*/
+void solve(){
+	return;
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+
+	
+
+	int t;
+	cin>>t;
+	while(t--)solve();
+	return 0;
+}
