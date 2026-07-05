2e107458aa46e0ac03749fe4eb6967f2fbfdfccf
Stefanos Stefanou <stefanos.stefanou@century.tech>
2026-07-05 21:38:57 +0100

    feat: draft: Usaco bronze stuck in a rut

---
+++ usaco/bronze/01 - simulation/06  - stuck_in_a_rut/stuck.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <math.h>
+#include <map>
+using namespace std;
+#define ll 		long long
+#define ull 	unsigned long long
+#define ld 		long double
+#define MOD  	pow(10,9)+7;
+#define INF_INT	(1<<30)
+#define INF_LL 	(1L<<62)
+#define ORIENTATION 0
+#define POS_X 1
+#define POS_Y 2
+#define IS_LOCKED 3
+#define GRASS_EATEN 4 
+
+//forward refs
+template <typename container> void debug(container& genericSequence,string id="None", int depth=0);
+
+//debug utils
+#ifdef DEBUG
+	template <typename container> void debug(container& genericSequence,string id, int depth){
+		cout<<"=============Debug ("<<id<<") START =============\n";
+		string prefix = "";
+		for(int i=0;i<depth;i++) prefix+="\t";
+
+		for(auto every: genericSequence){
+			cout<<prefix<<every<<"\n";
+		}
+		cout<<"=============Debug ("<<id<<") END =============\n";
+
+	}
+#endif
+#ifndef DEBUG
+	template <typename container> void debug(container& genericSequence,string id, int depth){
+		return ;
+	}
+
+#endif
+
+/**
+ 	*
+	? Stuff to look for ->
+    * stay organised
+    * int overflows, array bounds, etc.
+    * special cases (n=1)?
+    * do something instead of nothing
+    * timebox your approach
+    * simple is better than complex
+    * n % mod = (n % mod + mod) % mod;
+    * long long instead of int
+    
+*/
+void solve(){
+	int n,x,y;
+	string o;
+	vector<array<int,5>> map;
+	std::map<pair<int,int>,int> rutted_blocks;
+	cin>>n;
+	for (size_t i = 0; i < n; i++)
+	{
+		cin>>o>>x>>y;
+		//N,E | x | y | locked or not locked | GRASS EATEN
+		map.push_back({o[0],x,y,false,0});
+	}
+	
+	for (size_t i = 0; i < n; i++)
+	{
+		cout<<(char)map[i][0]<<"\t"<<map[i][1]<<"\t"<<map[i][2]<<"\n";
+	}
+	int hours=0;
+	int locked_cows=0;
+	
+	while(locked_cows<n && hours<500){
+		locked_cows=0;
+		hours+=1;
+		//for to update positions
+		for (size_t i = 0; i < n; i++)
+		{
+			if(map[i][IS_LOCKED]){
+				locked_cows++;
+				continue;
+			}//not blocked
+			if(map[i][ORIENTATION]=='E'){ //goes east
+				map[i][POS_X]+=1;
+			}
+			else{ //goes north
+				map[i][POS_Y]+=1;
+			}
+			rutted_blocks.insert({std::make_pair(map[i][POS_X],map[i][POS_Y]),1});
+
+		}
+		for (size_t i = 0; i < n; i++)
+		{
+			if(rutted_blocks.find(std::make_pair(map[i][POS_X],map[i][POS_Y]))==rutted_blocks.end()){
+				map[i][IS_LOCKED]=true;
+			}
+			else{
+				map[i][GRASS_EATEN]+=1;
+			}
+			
+		}
+	}
+	for (size_t i = 0; i < n; i++)
+	{	
+		if(!map[i][IS_LOCKED]){
+			cout<<"Infinity\n";
+		}
+		else{
+			cout<<map[i][GRASS_EATEN]<<"\n";
+		}
+	}
+	
+	
+	
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//freopen("input.in", "r", stdin);
+	//freopen("output.out", "w", stdout);
+	solve();
+	return 0;
+}
