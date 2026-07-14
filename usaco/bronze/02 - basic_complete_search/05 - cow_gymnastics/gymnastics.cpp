e00dd8c56986cbec0cb87cf862edf6952ab7325a
Stefanos Stefanou <stefanos.stefanou@century.tech>
2026-07-14 06:07:22 +0100

    feat: Cow Gymnastics

---
+++ usaco/bronze/02 - basic_complete_search/05 - cow_gymnastics/gymnastics.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <math.h>
+using namespace std;
+#define ll 		long long
+#define ull 	unsigned long long
+#define ld 		long double
+#define MOD  	pow(10,9)+7;
+#define INF_INT	(1<<30)
+#define INF_LL 	(1L<<62)
+
+//forward refs
+template <typename container> void debug(container& genericSequence,string id="None", int depth=0);
+
+//debug utils
+#ifdef DEBUG
+	template <typename container> void debug(container& genericSequence,string id, int depth){
+		cout<<"=============Debug ("<<id<<") START =============\n";
+		string prefix = "";
+		for(int i=0;i<depth;i++) prefix+="\t";
+
+		for(auto every: genericSequence){
+			cout<<prefix<<every<<"\n";
+		}
+		cout<<"=============Debug ("<<id<<") END =============\n";
+
+	}
+#endif
+#ifndef DEBUG
+	template <typename container> void debug(container& genericSequence,string id, int depth){
+		return ;
+	}
+
+#endif
+
+/**
+ 	*
+	? Stuff to look for ->
+    * stay organised
+    * int overflows, array bounds, etc.
+    * special cases (n=1)?
+    * do something instead of nothing
+    * timebox your approach
+    * simple is better than complex
+    * n % mod = (n % mod + mod) % mod;
+    * long long instead of int
+    
+*/
+void solve(){
+	ll k,n,tmp;
+	cin>>k>>n;
+
+	vector<vector<int>> runs;
+	for (size_t i = 0; i < k; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			cin>>tmp;
+			if(j==0)runs.push_back(std::vector<int>());
+			runs[j].push_back(tmp);
+		}	
+	}
+	
+	for (size_t i = 0; i < k; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			cout<<runs[i][j]<<"\t";
+		}
+		cout<<"\n";
+		
+	}
+	
+
+
+
+
+
+
+	return;
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//freopen("input.in", "r", stdin);
+	//freopen("output.out", "w", stdout);
+	solve();
+	return 0;
+}
