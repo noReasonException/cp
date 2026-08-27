c054a3b5df90287d0493026739cf259f0144d53b
Stefanos Stefanou <stefanos.stefanou@century.tech>
2026-08-27 09:17:47 +0100

    feat: leetcode DSA: two pointers

---
+++ leetcode/problems/344-reverse-string/reverse.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <math.h>
+using namespace std;
+#define ll 		long long
+#define ull 	unsigned long long
+#define ld 		long double
+#define MOD  	pow(10,9)+7;
+#define INF_INT	(1<<30)
+#define INF_LL 	(1L<<62)
+
+//forward refs
+template <typename container> void debug(container& genericSequence,string id="None", int depth=0);
+
+//debug utils
+#ifdef DEBUG
+	template <typename container> void debug(container& genericSequence,string id, int depth){
+		cout<<"=============Debug ("<<id<<") START =============\n";
+		string prefix = "";
+		for(int i=0;i<depth;i++) prefix+="\t";
+
+		for(auto every: genericSequence){
+			cout<<prefix<<every<<"\n";
+		}
+		cout<<"=============Debug ("<<id<<") END =============\n";
+
+	}
+#endif
+#ifndef DEBUG
+	template <typename container> void debug(container& genericSequence,string id, int depth){
+		return ;
+	}
+
+#endif
+
+/**
+ 	*
+	? Stuff to look for ->
+    * stay organised
+    * int overflows, array bounds, etc.
+    * special cases (n=1)?
+    * do something instead of nothing
+    * timebox your approach
+    * simple is better than complex
+    * n % mod = (n % mod + mod) % mod;
+    * long long instead of int
+    
+*/
+void reverse_str(vector<char>& str) {
+        int i=0;
+        int j=str.size()-1;
+        char a,b;
+        while(i<j){
+            a=str[i];
+            b=str[j];
+            str[i]=b;
+            str[j]=a;
+            i++;
+            j--;
+        }
+    }
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//freopen("input.in", "r", stdin);
+	//freopen("output.out", "w", stdout);
+	vector<char> e={'h','e','l','l','o'};
+	reverse_str(e);
+	for (size_t i = 0; i < e.size(); i++)
+	{
+		cout<<e[i];
+	}
+	
+
+	return 0;
+}
